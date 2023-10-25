f95afa706bc00072c785a1d66325641b9456d570
[Mac-Mini] Liam Fernandez <liamdfernandez@gmail.com>
2023-10-24 23:05:51 -0400

    remove bs files

--- src/lib/components/AddButton.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import CircledPlus from "$lib/icons/CircledPlus.svelte";
-</script>
-<button class="h-[222px] w-[271px] bg-[#45423F] flex justify-center rounded-2xl items-center">
-  <CircledPlus />
-</button>
